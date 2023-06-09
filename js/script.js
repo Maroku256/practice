67216ef92a3725bd8f8e9b98d201071f356fb5b6
Maroku256 <81808337+Maroku256@users.noreply.github.com>
2023-06-09 12:35:46 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let newsItems = document.querySelectorAll(".news-item")
+
+function changeYear(item, year){
+ for (let date of item){
+    if (date == year){
+        news-item.classList.add("hidden")
+    }else{
+
+    }
+ }
+}
